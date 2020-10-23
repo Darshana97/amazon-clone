f9a1a3ed095706ea91956578384b9a03fef3d98b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-23 17:09:50 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon clone</h1>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+              <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+              <h1>Login</h1>
+          </Route>
+          <Route path="/">
+              <h1>Home Page</h1>
+          </Route>
+        </Switch>
     </div>
+    </Router>
+    
   );
 }
 
