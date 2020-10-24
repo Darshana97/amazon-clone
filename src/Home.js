96babf57f02937ad179f05e0b7d1d4b06f32f128
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-24 15:34:47 +0530

    products

--- src/Home.js
+++ src/Home.js
@@ -11,13 +11,54 @@ function Home() {
         className="home__image"
       />
 
-      <Product
-        id={12345}
-        title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
-        price={11.96}
-        rating={5}
-        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-      />
+      <div className="home__row">
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+      </div> 
+      <div className="home__row">
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id={12345}
+          title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+        />
+      </div>
     </div>
   );
 }
