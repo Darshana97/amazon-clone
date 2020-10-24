926b5364f8e642b8fb885573bf06d5499c79252c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-24 13:57:24 +0530

    products

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
       <img
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/TheGentlemen/GENTL_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB404268992_.jpg"
-        alt="d dsvdv "
+        alt=""
         className="home__image"
+      />
+
+      <Product
+        id={12345}
+        title="The Lean Startup: How constant innovation creates redically successful businesspaperback"
+        price={11.96}
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
       />
     </div>
   );
