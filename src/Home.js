b99cdb2dc92ad0cde75006bcbcc43f447e44c6dc
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-24 13:11:04 +0530

    banner

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div>
-            <h1>I am from Home</h1>
-        </div>
-    )
+  return (
+    <div className="home">
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/TheGentlemen/GENTL_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB404268992_.jpg"
+        alt="d dsvdv "
+        className="home__image"
+      />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
